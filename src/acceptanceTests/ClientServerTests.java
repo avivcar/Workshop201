480a950ee4f46912e8ced23d78b220a4b35db7b7
mtros <mtros91@gmail.com>
2014-05-11 01:31:22 +0300

    testing changes

---
+++ src/acceptanceTests/ClientServerTests.java
@@ -0,0 +1,15 @@
+package acceptanceTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	initReactorTest.class,
+	ReactorTests.class,
+	
+})
+public class ClientServerTests {
+
+}
---
+++ src/acceptanceTests/ReactorTests.java
@@ -0,0 +1,17 @@
+package acceptanceTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReactorTests {
+
+	public ReactorTests() {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
