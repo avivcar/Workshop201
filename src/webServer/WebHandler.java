5f86366e693f1b02be023851f49e4cf9166ceb29
Astralicon <matanoga@gmail.com>
2014-06-09 14:52:08 +0300

    ddd

--- src/webServer/WebHandler.java
+++ src/webServer/WebHandler.java
@@ -32,28 +32,46 @@ public class WebHandler implements HttpHandler {
 	}
 
 	public void handle(HttpExchange request) throws IOException {
-        String response = this.generateResponse(new httpRequest(request, this.BASE_PATH));
-        Headers respHeaders = request.getResponseHeaders();
+		httpRequest httpReq = new httpRequest(request, this.BASE_PATH);
+		User user = validateUser(httpReq); //checking if the user is logged in
+		String response = "Not logged in";
+		
+		//not logged in
+		if (user == null) {
+			if (httpReq.hasPost("username") && httpReq.hasPost("password") && httpReq.hasPost("forumId")) {
+				user = system.login(httpReq.getPost("username"),httpReq.getPost("password"), httpReq.getPost("forumId"));
+				String sessId = generateSessionId();
+				this.sessions.put(sessId, user);
+				httpReq.setSessionId(sessId);
+			}	
+		}
+		
+		//if user exists and logged in, get response. 
+		//if the user just logged in in this request, need to return sub forums.
+		if (user != null) response = WebProtocol.getResponse(httpReq, user, system);
+		
+		//headers and cookies
+		Headers respHeaders = request.getResponseHeaders();
         request.sendResponseHeaders(200, response.length());
         OutputStream os = request.getResponseBody();
         os.write(response.getBytes());
         os.close();
 	}
 	
-	public String generateResponse(httpRequest request) {
-		User user = validateUser(request);
+	/*public String generateResponse(httpRequest request) {
 		
 		
+		String ans = WebProtocol.getResponse(request);
 		// login request
 		if (!(request.hasPost("username") && request.hasPost("password") && request.hasPost("forumId"))) return "error";
 		user = new User(); //system.login(request.getPost("username"),request.getPost("password"), request.getPost("forumId"));
-		if (user == null) return "error";
+		if (user == null) return "Not logged in";
 		String sessId = generateSessionId();
 		this.sessions.put(sessId, user);
 		request.setSessionId(sessId);
 		
 		return "cool";
-	}
+	}*/
 	
 	public User validateUser(httpRequest request) {
 		String sessId = request.getSessionId();
--- src/webServer/WebProtocol.java
+++ src/webServer/WebProtocol.java
@@ -1,7 +1,30 @@
 package webServer;
 
+import forumSystemCore.ForumSystem;
+import user.User;
+
 public class WebProtocol {
 	
-	
+	public static String getResponse(httpRequest request, User user, ForumSystem sys) {
+		String ans = "Command not found. sorry!";
+		String pageReq = request.getPath();
+		switch (pageReq) {
+			case "login":
+				break;
+			//forums req
+			case "index":
+			case "":
+				break;
+			//get forum subforums
+			case "forum":
+				break;
+			//get subforum messages
+			case "subforum":
+				break;
+			//get 
+		}
+		
+		return ans;
+	}
 	
 }
