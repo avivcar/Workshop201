590ea2ad7050960550727bf395b8830cc531f4e8
avivcar <avivcar@post.bgu.ac.il>
2014-06-11 16:09:34 +0300

    rwe

--- src/webServer/WebProtocol.java
+++ src/webServer/WebProtocol.java
@@ -15,16 +15,21 @@ public class WebProtocol {
 	
 	public static String getResponse(httpRequest request, User user, ForumSystem sys) {
 		if (request.hasPost("sideEffect")) {
-			if ((request.getPost("sideEffect").equals("login") || request.getPost("sideEffect").equals("login")) && user == null) return getHeader(user) + "<h2 style=\"color: red;\">Sorry, the details you entered were incorrect, please try again</h2>" + echoHomepage(sys) + getFooter();
+			if ((request.getPost("sideEffect").equals("login") || request.getPost("sideEffect").equals("login")) && user == null) return getHeader(user) + "<div style=\"color: red;\">Sorry, the details you entered were incorrect, please try again</div>" + echoHomepage(sys) + getFooter();
 			if (request.getPost("sideEffect").equals("addReply")) {
 				if (!request.hasPost("id")) return getHeader(user) + getRedirect("/forum") + getFooter();
-				if (!request.hasPost("title") || !request.hasPost("content")) return getHeader(user) + getRedirect("/reply?id=" + request.getPost("id")) + getFooter();
+				if (!request.hasPost("title") || !request.hasPost("content")) return getHeader(user) + "<div style=\"color: red;\">You must supply at least title or content</div>" + getRedirect("/reply?id=" + request.getPost("id")) + getFooter();
 				sys.getMessageById(request.getPost("id")).addReply(user, request.getPost("title"), request.getPost("content"));
 			}
 			if (request.getPost("sideEffect").equals("addMessage")) {
 				if (!request.hasPost("id")) return getHeader(user) + getRedirect("/forum") + getFooter();
-				if (!request.hasPost("title") || !request.hasPost("content")) return getHeader(user) + getRedirect("/add?id=" + request.getPost("id")) + getFooter();
+				if (!request.hasPost("title") || !request.hasPost("content")) return getHeader(user) + "<div style=\"color: red;\">You must supply at least title or content</div>" + getRedirect("/add?id=" + request.getPost("id")) + getFooter();
 				sys.getSubForumById(request.getPost("id")).createMessage(user, request.getPost("title"), request.getPost("content"));
+			}
+			if (request.getPost("sideEffect").equals("editMessage")) {
+				if (!request.hasPost("id")) return getHeader(user) + getRedirect("/forum") + getFooter();
+				if (!request.hasPost("title") || !request.hasPost("content")) return getHeader(user) + "<div style=\"color: red;\">You must supply at least title or content</div>" + getRedirect("/edit?id=" + request.getPost("id")) + getFooter();
+				sys.getMessageById(request.getPost("id")).editMessage(user, request.getPost("title"), request.getPost("content"));
 			}
 		}
 		String ans = getHeader(user);
@@ -43,11 +48,11 @@ public class WebProtocol {
 			//get subforum messages
 			case "subforum":
 				if (!request.hasGet("id")) ans += getRedirect("/forum");
-				else ans += echoSubForum(sys.getSubForumById(request.getGet("id")), sys);
+				else ans += echoSubForum(sys.getSubForumById(request.getGet("id")), sys, user);
 				break;
 			case "message":
 				if (!request.hasGet("id")) ans += getRedirect("/forum");
-				else ans += echoMsg(sys.getMessageById(request.getGet("id")), sys);
+				else ans += echoMsg(sys.getMessageById(request.getGet("id")), sys, user);
 				break;
 			case "login":
 				if (!request.hasGet("id")) ans += getRedirect("/forum");
@@ -58,12 +63,24 @@ public class WebProtocol {
 				else ans += echoSignup(request.getGet("id"));
 				break;
 			case "add":
+				if (user == null) return getHeader(user) + "<div style=\"color: red;\">You must be loggin in!</div>" + echoHomepage(sys) + getFooter();
 				if (!request.hasGet("id")) ans += getRedirect("/forum");
 				else ans += echoAddMessage(request.getGet("id"));
 				break;
 			case "reply":
+				if (user == null) return getHeader(user) + "<div style=\"color: red;\">You must be loggin in!</div>" + echoHomepage(sys) + getFooter();
 				if (!request.hasGet("id")) ans += getRedirect("/forum");
 				else ans += echoAddReply(request.getGet("id"));
+				break;
+			case "edit":
+				if (user == null) return getHeader(user) + "<div style=\"color: red;\">You must be loggin in!</div>" + echoHomepage(sys) + getFooter();
+				if (!request.hasGet("id")) ans += getRedirect("/forum");
+				else {
+					String msgId = request.getGet("id");
+					Message msg = sys.getMessageById(msgId);
+					if (msg == null) ans += getRedirect("/forum");
+					else ans += echoEditMsg(msg);
+				}
 				break;
 			//get 
 		}
@@ -75,7 +92,7 @@ public class WebProtocol {
 	
 	//The builders of this wonderful facility, full factility. i swear.
 	private static String echoHomepage(ForumSystem sys){
-		String ans = "</span></div><div style=\"clear: both;\"></div><h2>Forum System</h2>";
+		String ans = "<div id=\"bread-crumbs\"><span>Yakuni Forums System</span></div><div style=\"clear: both;\"></div>";
 		for (int i=0; i < sys.forums.size(); i++) {
 			ans += "<div><a class=\"item\"  href=\"/forum/login?id=" + sys.forums.get(i).getId() + "\">" + sys.forums.get(i).getName() + "</a></div>";
 		}
@@ -84,7 +101,7 @@ public class WebProtocol {
 	
 	
 	private static String echoLogin(String forumId){
-		String ans = "</span></div><div style=\"clear: both;\"></div><h2>Login</h2>";
+		String ans = "</span></div><div style=\"clear: both;\"></div><h2>Login | <a href=\"/forum/forum?id=" + forumId + "\">Continue as guest</a></h2>";
 		ans += "<form action=\"/forum/forum?id=" + forumId + "\" method=\"post\">";
 		ans += "<input type=\"hidden\" name=\"sideEffect\" value=\"login\">";
 		ans += "<div><label>Username:</label><input type=\"text\" name=\"username\"></div>";
@@ -105,6 +122,21 @@ public class WebProtocol {
 		ans += "<div><label>Title:</label><input type=\"text\" name=\"title\"></div>";
 		ans += "<div><label>Content:</label></div>";
 		ans += "<textarea name=\"content\"></textarea>";
+    	ans += "<button>Subimt</button>";
+    	ans += "</form>";
+    	return ans;
+	}
+	
+	
+	private static String echoEditMsg(Message msg){
+		String msgId = msg.getId();
+		String ans = "</span></div><div style=\"clear: both;\"></div><h2>Add Reply</h2>";
+		ans += "<form action=\"/forum/message?id=" + msgId + "\" method=\"post\">";
+		ans += "<input type=\"hidden\" name=\"sideEffect\" value=\"editMessage\">";
+		ans += "<input type=\"hidden\" name=\"id\" value=\"" + msgId + "\">";
+		ans += "<div><label>Title:</label><input type=\"text\" name=\"title\" value=\"" + msg.getTitle() + "\"></div>";
+		ans += "<div><label>Content:</label></div>";
+		ans += "<textarea name=\"content\">" + msg.getContent() + "</textarea>";
     	ans += "<button>Subimt</button>";
     	ans += "</form>";
     	return ans;
@@ -140,39 +172,38 @@ public class WebProtocol {
 	}
 	
 	
-	private static String echoMsg(Message msg, ForumSystem sys){
+	private static String echoMsg(Message msg, ForumSystem sys, User user){
 		if (msg == null) return getRedirect("/forum/");
 		Message parent = msg;
 		while (parent.getSubforumId().equals("0")) parent = sys.getMessageById(msg.getMsgRel());
 		SubForum fs = sys.getSubForumById(parent.getSubforumId());
 		Forum f = sys.getForum(fs.getForumId());
-		String ans = "<div id=\"bread-crumbs\"><a href=\"/forum/forum?id=" + f.getId() + "\">" + f.getName() + "</a> >> <a href=\"" + fs.getId() + "\">" + fs.getSubject() + "</a> >> <span>" + msg.getTitle() + "</span></div><div style=\"clear: both;\"></div>";
+		String ans = "<div id=\"bread-crumbs\"><a href=\"/forum\">Yakuni Forums System</a> >> <a href=\"/forum/forum?id=" + f.getId() + "\">" + f.getName() + "</a> >> <a href=\"" + fs.getId() + "\">" + fs.getSubject() + "</a> >> <span>" + (msg.getTitle().equals("") ? "Untitled Message" : msg.getTitle()) + "</span></div><div style=\"clear: both;\"></div>";
 		ans += "<h2>" + (msg.getTitle().equals("") ? "Untitled Message" : msg.getTitle()) + "</h2><div>" + msg.getContent() + "</div><h2>Replies:</h2>";
 		for (int i=0; i<msg.getReplies().size(); i++) {
 			ans += "<div><a class=\"item\"  href=\"/forum/message?id=" + msg.getReplies().get(i).getId() + "\">" + (msg.getReplies().get(i).getTitle().equals("") ? "Untitled Message" : msg.getReplies().get(i).getTitle()) + "</a></div>";
 		}
-		ans += "<h2><a href=\"/forum/reply?id=" + msg.getId() + "\">Add Reply</a></h2>";
+		if (msg.getUser() == user) ans += "<h2><a href=\"/forum/reply?id=" + msg.getId() + "\">Add Reply</a></h2>";
 		return ans;
 	}
 	
 	private static String echoForum(Forum forum) {
 		if (forum == null) return getRedirect("/forum/");
-		String ans = "</span></div><div style=\"clear: both;\"></div><h2>Forum - " + forum.getName() + "</h2>";
+		String ans = "<div id=\"bread-crumbs\"><a href=\"/forum\">Yakuni Forums System</a> >> <span>Forum - " + forum.getName() + "</span></div><div style=\"clear: both;\"></div>";
 		for (int i=0; i < forum.getSubForums().size(); i++) {
 			ans += "<div><a class=\"item\"  href=\"/forum/subforum?id=" + forum.getSubForums().get(i).getId() + "\">" + forum.getSubForums().get(i).getSubject() + "</a></div>";
 		}
 		return ans;
 	}
 	
-	private static String echoSubForum(SubForum subForum, ForumSystem sys) {
+	private static String echoSubForum(SubForum subForum, ForumSystem sys, User user) {
 		Forum f = sys.getForum(subForum.getForumId());
 		if (subForum == null) return getRedirect("/forum/");
-		String ans = "<div id=\"bread-crumbs\"><a href=\"/forum/forum?id=" + f.getId() + "\">" + f.getName() + "<span>" + subForum.getSubject() + "</span></div><div style=\"clear: both;\"></div>";
-		ans += "<h2>SubForum - " + subForum.getSubject() + "</h2>";
+		String ans = "<div id=\"bread-crumbs\"><a href=\"/forum\">Yakuni Forums System</a> >> <a href=\"/forum/forum?id=" + f.getId() + "\">" + f.getName() + "</a> >> <span>" + subForum.getSubject() + "</span></div><div style=\"clear: both;\"></div>";
 		for (int i=0; i < subForum.getMessages().size(); i++) {
 			ans += "<div><a class=\"item\"  href=\"/forum/message?id=" + subForum.getMessages().get(i).getId() + "\">" + subForum.getMessages().get(i).getTitle() + "</a></div>";
 		}
-		ans += "<h2><a href=\"/forum/add?id=" + subForum.getId() + "\">Add A New Thread</a></h2>";
+		if (sys.isMember(subForum.getForumId(), user)) ans += "<h2><a href=\"/forum/add?id=" + subForum.getId() + "\">Add A New Thread</a></h2>";
 		return ans;
 	}
 	
